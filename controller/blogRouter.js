c2910d93b5d0edbe9798b01d0bb0bddcd93f181f
abhishekgg14 <abhishekgireesh5655@gmail.com>
2024-02-09 11:16:27 +0530

    password encryption

--- controller/blogRouter.js
+++ controller/blogRouter.js
@@ -2,9 +2,24 @@ const express=require("express")
 const usermodel=require("../models/userModel")
 
 const router=express.Router()
+const bcrypt=require("bcryptjs")
+
+hashPasswordGenerator=async(password)=>{
+    const salt=await bcrypt.genSalt(10)
+    return bcrypt.hash(password,salt)
+}
 
 router.post("/signup",async(req,res)=>{
-    let data=req.body
+    let {data}={"data":req.body}
+    let pass=data.pass
+    hashPasswordGenerator(pass).then(
+        (hashedPassword)=>{
+            console.log(hashedPassword)
+            data.pass=hashedPassword
+        }
+    )
+    console.log(data)
+    
     let details=new usermodel(data)
     let result=await details.save()
     res.json({
